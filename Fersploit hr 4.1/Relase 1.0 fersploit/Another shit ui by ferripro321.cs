dc4c70e86f19c687d125d7cc1f0392206ea25603
Ferripro321 <54151517+Ferripro321@users.noreply.github.com>
2020-07-12 15:08:10 +0200

    Create Another shit ui by ferripro321.cs

---
+++ Fersploit hr 4.1/Relase 1.0 fersploit/Another shit ui by ferripro321.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Relase_1._0_fersploit
+{
+    public partial class Another_shit_ui_by_ferripro321 : Form
+    {
+        public Another_shit_ui_by_ferripro321()
+        {
+            InitializeComponent();
+        }
+    }
+}
